492c0c91c64720347a2a07d362649057d042b4d1
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-18 23:32:08 -0700

    ✔ done setting showing list  after  loading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { useFetch } from './useFetch';
 import Follower from './Follower';
 function App() {
    const { loading, data } = useFetch();
+   const [page, setPage] = useState(0);
+   const [followers, setFollowers] = useState([]);
+
+   useEffect(() => {
+      //  don't get data when is loading get it after loading
+      if (loading) return;
+      setFollowers(data[page]);
+      // when loading changes that we set on second param
+   }, [loading]);
+
    return (
       <main>
          {/* setting title to change from loading to pagination */}
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,21 +1,24 @@
-import { useState, useEffect } from 'react'
-import paginate from './utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+import { useState, useEffect } from 'react';
+import paginate from './utils';
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
 
 export const useFetch = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
+   const [loading, setLoading] = useState(true);
+   const [data, setData] = useState([]);
 
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    setData(data)
-    paginate(data)
-    setLoading(false)
-  }
+   const getProducts = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      // pass in the data from pagination in utils.js
+      // which is 12 arrays of data now the data on line 7 is set to 12 arrays of users 
+      
+      setData(paginate(data));
 
-  useEffect(() => {
-    getProducts()
-  }, [])
-  return { loading, data }
-}
+      setLoading(false);
+   };
+
+   useEffect(() => {
+      getProducts();
+   }, []);
+   return { loading, data };
+};
