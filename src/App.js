363c60ab36ef888ab2d4c2db152939eec87daebe
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-18 23:58:08 -0700

    project done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,31 @@ function App() {
       if (loading) return;
       setFollowers(data[page]);
       // when loading changes that we set on second param
-   }, [loading]);
+      // also everytime page changes use useEffect(() =>)
+   }, [loading, page]);
+
+   //  setpage by passing in index to page
+   const handlePage = (index) => {
+      setPage(index);
+   };
+   const nextPage = () => {
+      setPage((oldpage) => {
+         let nextPage = oldpage + 1;
+         if (nextPage > data.length - 1) {
+            nextPage = 0;
+         }
+         return nextPage;
+      });
+   };
+   const prevPage = () => {
+      setPage((oldpage) => {
+         let prevPage = oldpage - 1;
+         if (prevPage < 0) {
+            prevPage = data.length - 1;
+         }
+         return prevPage;
+      });
+   };
 
    return (
       <main>
@@ -25,10 +49,36 @@ function App() {
          {/* display followers */}
          <section className="followers">
             <div className="container">
-               {data.map((follower) => {
+               {followers.map((follower) => {
                   return <Follower key={follower.id} {...follower} />;
                })}
             </div>
+            {/* setup showing container after the page has load  */}
+            {!loading && (
+               <div className="btn-container">
+                  {/* // add prev page buttton */}
+                  <button className="prev-btn" onClick={prevPage}>
+                     Prev
+                  </button>
+                  {/* return page amount */}
+                  {data.map((follower, index) => {
+                     return (
+                        <button
+                           className={`page-btn ${
+                              index === page ? 'active-btn' : null
+                           }`}
+                           key={index}
+                           onClick={() => handlePage(index)}
+                        >
+                           {index + 1}
+                        </button>
+                     );
+                  })}
+                  <button className="next-btn" onClick={nextPage}>
+                     Prev
+                  </button>
+               </div>
+            )}
          </section>
       </main>
    );
