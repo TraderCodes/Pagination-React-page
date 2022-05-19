f65721bebd5283bb1bc0ae870a4920d730fd71f6
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-18 23:21:35 -0700

    ✔ Done setting up items perpage using slick / Array.from

--- src/useFetch.js
+++ src/useFetch.js
@@ -10,6 +10,7 @@ export const useFetch = () => {
     const response = await fetch(url)
     const data = await response.json()
     setData(data)
+    paginate(data)
     setLoading(false)
   }
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,21 @@
-const paginate = () => {}
+// setup arrays in arrays
+const paginate = (followers) => {
+   const itemPerPage = 9;
+   //    find out how many page we need
+   //    round up the page so the rest can fit into the last page
+   const page = Math.ceil(followers.length / itemPerPage);
 
-export default paginate
+   //    set arrays
+   const newFollowers = Array.from({ length: page }, (_, index) => {
+      const start = index * itemPerPage;
+      //   first time the index will be 0
+
+      //   start slicing the followers from the main list base on out must itemPerPage
+      //   first param is the starting point second param is the ending
+      //  return the amount of items each time to run the function
+      return followers.slice(start, start + itemPerPage);
+   });
+   return newFollowers;
+};
+
+export default paginate;
